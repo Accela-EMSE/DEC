64e968f148e07134b97285b321d7af11a950184e
johnschomp <jschomp@accela.com>
2014-01-18 19:18:13 +0000

    17505

--- SCRIPTS/EXPRESSIONS/expression_Applicant_Deceased.js
+++ SCRIPTS/EXPRESSIONS/expression_Applicant_Deceased.js
@@ -1,18 +1,18 @@
-var deceasedDate=expression.getValue("APPLICANT::applicant*deceasedDate");
+//var deceasedDate=expression.getValue("APPLICANT::applicant*deceasedDate");
 var mailStop=expression.getValue("APPLICANTTPLFORM::CNT_MASTER::ADDITIONAL INFO::Stop Mail");
 var thisForm=expression.getValue("APPLICANT::FORM");
 var capTypeExpr=expression.getValue("CAP::capType");
 
-deceasedDate.hidden = true;
+//deceasedDate.hidden = true;
 
 if (capTypeExpr.getValue() != "Licenses/DEC Internal/Enforcement/Enforcement Request") {
 	var stopSale = false;
 	var blockMessage = "Cannot continue sale, selected applicant ";
 
-	if (deceasedDate.getValue() != null && deceasedDate.getValue() != "" && deceasedDate.getValue() != undefined) {
-		stopSale = true;
-		blockMessage += "is deceased.";
-	}
+//	if (deceasedDate.getValue() != null && deceasedDate.getValue() != "" && deceasedDate.getValue() != undefined) {
+//		stopSale = true;
+//		blockMessage += "is deceased.";
+//	}
 
 	if (mailStop.getValue() == "Y" || mailStop.getValue() == "Yes") {
 		stopSale = true;
