a0da9499e22c14174222c9a025ec62355de133d6
koteswar <koteswar@eddbf159-744f-4d5c-8869-80d0479a017d>
2014-02-18 19:46:00 +0000

    Initialize Batch Scripts

---
+++ SCRIPTS/BATCH/BATCH_MAINTAIN_LIC_YEAR.js
@@ -0,0 +1,142 @@
+/*------------------------------------------------------------------------------------------------------/
+| Program:  BATCH_MAINTAIN_LIC_YEAR.js  Trigger: Batch
+| Event   : N/A
+| Usage   : Batch Script  
+| Agency  : DEC
+| Purpose : To Maintain list for LICENSE_FILING_YEAR and LICENSE_FILING_YEAR_Desc which are used in sales.
+| Notes   : 07/15/2013,     Lalit S Gawad (LGAWAD),     Initial Version 
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+| START: TEST PARAMETERS
+/------------------------------------------------------------------------------------------------------*/
+//aa.env.setValue("emailAddress", "");
+//aa.env.setValue("showDebug", "Y");
+/*------------------------------------------------------------------------------------------------------/
+| END: TEST PARAMETERS
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+| START: USER CONFIGURABLE PARAMETERS
+/------------------------------------------------------------------------------------------------------*/
+var emailText = "";
+var maxSeconds = 4.5 * 60; 	    // number of seconds allowed for batch processing, usually < 5*60
+var message = "";
+var br = "<br>";
+/*------------------------------------------------------------------------------------------------------/
+| END: USER CONFIGURABLE PARAMETERS
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+| BEGIN Includes
+/------------------------------------------------------------------------------------------------------*/
+SCRIPT_VERSION = 2.0
+eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
+eval(getScriptText("INCLUDES_BATCH"));
+eval(getScriptText("INCLUDES_CUSTOM"));
+
+function getScriptText(vScriptName) {
+    vScriptName = vScriptName.toUpperCase();
+    var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+    var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+    return emseScript.getScriptText() + "";
+}
+/*------------------------------------------------------------------------------------------------------/
+| START: BATCH PARAMETERS
+/------------------------------------------------------------------------------------------------------*/
+var emailAddress = getParam("emailAddress"); 				// email to send report
+/*------------------------------------------------------------------------------------------------------/
+| END: BATCH PARAMETERS
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+| START: Variable Definitions
+/------------------------------------------------------------------------------------------------------*/
+var servProvCode = aa.getServiceProviderCode();
+var showDebug = isNull(aa.env.getValue("showDebug"), "N") == "Y";
+var batchJobID = 0;
+var batchJobName = "";
+var batchJobDesc = "";
+var batchJobResult = "";
+var systemUserObj = aa.person.getUser("ADMIN").getOutput();
+var sysDate = aa.date.getCurrentDate();
+var currentUser = aa.person.getCurrentUser().getOutput();
+var startDate = new Date();
+var startTime = startDate.getTime(); 		// Start timer
+var appTypeArray = new Array();
+var showDebug = isNull(aa.env.getValue("showDebug"), "N") == "Y";
+/*------------------------------------------------------------------------------------------------------/
+| END: Variable Definitions
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+| START: MAIN LOGIC
+/-----------------------------------------------------------------------------------------------------*/
+var isPartialSuccess = false;
+var timeExpired = false;
+
+logDebug("Start of Job");
+if (!timeExpired) var isSuccess = mainProcess();
+logDebug("End of Job: Elapsed Time : " + elapsed() + " Seconds");
+if (isSuccess) {
+    aa.print("Passed");
+    aa.env.setValue("ScriptReturnCode", "0");
+    if (isPartialSuccess) {
+        aa.env.setValue("ScriptReturnMessage", "A script timeout has caused partial completion of this process.  Please re-run.");
+        aa.eventLog.createEventLog("Batch Job run partial successful.", "Batch Process", batchJobName, sysDate, sysDate, batchJobDesc, batchJobResult, batchJobID);
+    } else {
+        aa.env.setValue("ScriptReturnMessage", "Batch Job run successfully.");
+        aa.eventLog.createEventLog("Batch Job run successfully.", "Batch Process", batchJobName, sysDate, sysDate, batchJobDesc, batchJobResult, batchJobID);
+    }
+}
+else {
+    aa.print("Failed");
+    aa.env.setValue("ScriptReturnCode", "1");
+    aa.env.setValue("ScriptReturnMessage", "Batch Job failed: " + emailText);
+}
+
+if (emailAddress.length)
+    aa.sendMail("noreply@accela.com", emailAddress, "", batchJobName + " Results", emailText);
+/*------------------------------------------------------------------------------------------------------/
+| END: MAIN LOGIC
+/-----------------------------------------------------------------------------------------------------*/
+
+function mainProcess() {
+    var vError = null;
+    try {
+        var vSuccess = checkBatch();
+        if (!vSuccess) return false;
+
+        logDebug("****** Start logic ******");
+
+        if (elapsed() > maxSeconds) // only continue if time hasn't expired
+        {
+            isPartialSuccess = true;
+            showDebug = true;
+            logDebug("A script timeout has caused partial completion of this process.  Please re-run.  " + elapsed() + " seconds elapsed, " + maxSeconds + " allowed.");
+            timeExpired = true;
+            return false;
+        }
+
+        //Following function        is in INCLUDES_DEC_MANAGE_STD_CHOICE
+        InitLicenseFilingYear();
+
+        logDebug("****** End logic ******");
+
+        return vSuccess;
+    }
+    catch (vError) {
+        logDebug("Runtime error occurred: " + vError);
+        return true;
+    }
+}
+
+function checkBatch() {
+    var vBatchJobResult = aa.batchJob.getJobID();
+    batchJobName = "" + aa.env.getValue("BatchJobName");
+
+    if (vBatchJobResult.getSuccess()) {
+        batchJobID = vBatchJobResult.getOutput();
+        logDebug("Batch job ID found " + batchJobID);
+        return true;
+    }
+    else {
+        logDebug("Batch job ID not found " + vBatchJobResult.getErrorMessage());
+        return false;
+    }
+}
