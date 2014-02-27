15e1d5b8025863237e29227d9847e38d1135ac45
apritjain <apritjain@eddbf159-744f-4d5c-8869-80d0479a017d>
2014-02-27 19:08:10 +0000

    Expressions for Hunting

---
+++ SCRIPTS/EXPRESSIONS/expr_ASI_HUNT_SubmitAA.js
@@ -0,0 +1,37 @@
+var toPrecision=function(value){
+  var multiplier=10000;
+  return Math.round(value*multiplier)/multiplier;
+}
+function addDate(iDate, nDays){ 
+	if(isNaN(nDays)){
+		throw("Day is a invalid number!");
+	}
+	return expression.addDate(iDate,parseInt(nDays));
+}
+
+function diffDate(iDate1,iDate2){
+	return expression.diffDate(iDate1,iDate2);
+}
+
+function parseDate(dateString){
+	return expression.parseDate(dateString);
+}
+
+function formatDate(dateString,pattern){ 
+	if(dateString==null||dateString==''){
+		return '';
+	}
+	return expression.formatDate(dateString,pattern);
+}
+
+var servProvCode=expression.getValue("$$servProvCode$$").value;
+var variable0=expression.getValue("ASI::FORM");
+
+
+var totalRowCount = expression.getTotalRowCount();
+
+			variable0.blockSubmit=true;
+		expression.setReturn(variable0);
+
+			variable0.message="This application process is set to work with ACA only. Please use ACA to create new application.";
+		expression.setReturn(variable0);
