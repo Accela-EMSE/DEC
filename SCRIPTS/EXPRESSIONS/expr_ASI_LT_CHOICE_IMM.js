04d166468ab0ad4fbfda4c1ff1717fade311abc9
koteswar <koteswar@eddbf159-744f-4d5c-8869-80d0479a017d>
2014-02-27 20:59:50 +0000

    Initialize LifeTime Expression Scriptings

---
+++ SCRIPTS/EXPRESSIONS/expr_ASI_LT_CHOICE_IMM.js
@@ -0,0 +1,8 @@
+var servProvCode=expression.getValue("$$servProvCode$$").value;
+var onRenew =expression.getValue("ASI::LIFETIME LICENSES::Add Lifetime to Driver License on Renewal");
+var onImmed =expression.getValue("ASI::LIFETIME LICENSES::Add Lifetime to Driver License Re-Issue Immediately");
+
+if (onRenew.value == "CHECKED" && onImmed.value == "CHECKED") {
+  onRenew.value = "";
+  expression.setReturn(onRenew);
+  }
