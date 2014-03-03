50a83b451968c069790358e5a8107ba509145fc9
apritjain <apritjain@eddbf159-744f-4d5c-8869-80d0479a017d>
2014-03-03 17:26:15 +0000

    Radio Buttons Fields added in Expressions

--- SCRIPTS/EXPRESSIONS/expr_ASI_HUNT_5Yr_Bowhunting_Privl.js
+++ SCRIPTS/EXPRESSIONS/expr_ASI_HUNT_5Yr_Bowhunting_Privl.js
@@ -38,6 +38,9 @@ var aDriverLicenseNumber = expression.getValue("ASI::INTERNAL USE::A_Driver_Lice
 var aNonDriverLicenseNumber = expression.getValue("ASI::INTERNAL USE::A_Non_Driver_License_Number");
 var vUserID = expression.getValue("$$userID$$");
 var sUserIdEB = vUserID.getValue();
+var aIsMilitaryServiceman = expression.getValue("ASI::INTERNAL USE::A_Military Serviceman");
+var aIsLegallyBlind = expression.getValue("ASI::INTERNAL USE::A_Legally Blind");
+var aPermanentDisability = expression.getValue("ASI::INTERNAL USE::A_Permanent Disability");
 
 //Init 
 var f = new form_OBJECT(GS2_EXPR);
@@ -61,6 +64,8 @@ f.SetEnforcementAttrib(aSuspended.value, aRevokedHunting.value, aRevokedTrapping
 f.SetFulfillmentAttrib(aAgedIn.value, aNeedHuntEd.value);
 f.FromACA = aIsFromACA.value;
 f.UserIdEB = sUserIdEB;
+f.IsMilitaryServiceman = aIsMilitaryServiceman.value;
+f.IsLegallyBlind = aIsLegallyBlind.value;
 //
 
 //Set control array and set values for lic
@@ -81,8 +86,7 @@ f.ExecuteBoRuleEngine();
 
 
 ////Set Lic availablity using lic array from app object
-for (var idx = 0; idx < f.licObjARRAY.length; idx++) {
-    //var oTemp = new License_OBJ();
+for (var idx = 0; idx < f.licObjARRAY.length; idx++) {    //var oTemp = new License_OBJ();
     var oLic = f.licObjARRAY[idx];
 
     if (f.licObjARRAY[idx].IsSelectable == false || f.licObjARRAY[idx].IsActive == false) {
