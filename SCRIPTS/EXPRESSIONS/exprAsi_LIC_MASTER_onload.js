6476c83dfd08cd0b2f41699bfab20db7a0e3b432
koteswar <koteswar@eddbf159-744f-4d5c-8869-80d0479a017d>
2014-03-06 18:58:25 +0000

    Add Record Type LIC_MASTER

--- SCRIPTS/EXPRESSIONS/exprAsi_LIC_MASTER_onload.js
+++ SCRIPTS/EXPRESSIONS/exprAsi_LIC_MASTER_onload.js
@@ -104,6 +104,8 @@ var aDriverLicenseNumber = expression.getValue("ASI::INTERNAL USE::A_Driver_Lice
 var aNonDriverLicenseNumber = expression.getValue("ASI::INTERNAL USE::A_Non_Driver_License_Number");
 var vUserID = expression.getValue("$$userID$$");
 var sUserIdEB = vUserID.getValue();
+var oRecordType=expression.getValue("CAP::capType");
+
 
 //Init 
 var f = new form_OBJECT(GS2_EXPR, OPTZ_TYPE_ALLFEES);
@@ -135,6 +137,7 @@ f.SetEnforcementAttrib(aSuspended.value, aRevokedHunting.value, aRevokedTrapping
 f.SetFulfillmentAttrib(aAgedIn.value, aNeedHuntEd.value);
 f.FromACA = aIsFromACA.value;
 f.UserIdEB = sUserIdEB;
+f.RecordType = oRecordType.getValue();
 //
 
 //Set control array and set values for lic
