e375bfc319b3b0860ab0b7a54cbc7e69e26d6579
koteswar <koteswar@eddbf159-744f-4d5c-8869-80d0479a017d>
2014-03-12 18:47:17 +0000

    Initialize the ASI_TRANSFER onchage validations

---
+++ SCRIPTS/EXPRESSIONS/expr_ASI_TRANSFER_DecIDCheck.js
@@ -0,0 +1,31 @@
+var servProvCode = expression.getValue("$$servProvCode$$").value;
+var totalRowCount = expression.getTotalRowCount();
+
+var aa = expression.getScriptRoot();
+
+eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
+eval(getScriptText("INCLUDES_CUSTOM"));
+eval(getScriptText("INCLUDES_EXPRESSIONS"));
+
+function getScriptText(vScriptName) {
+    vScriptName = vScriptName.toUpperCase();
+    var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+    var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+    return emseScript.getScriptText() + "";
+}
+var aYear = expression.getValue("ASI::GENERAL INFORMATION::License Year");
+var vUserID = expression.getValue("$$userID$$");
+var sUserIdEB = vUserID.getValue();
+var oRecordType=expression.getValue("CAP::capType");
+
+var otDecId = expression.getValue("ASI::TRANSFER INFORMATION::Customer ID Transfer to (DEC ID)");
+//
+var isDecIDExist = getPeopleByDecID(otDecId);
+var msg = "";
+if (isDecIDExist==null) {
+    msg="Customer ID Transfer to (DEC ID)  is not exit";
+	otDecId.value = "";
+ } 
+otDecId.message = msg;
+expression.setReturn(otDecId);
+////
