8953b65a07d00991912b120d2eacc423c3611294
apritjain <apritjain@eddbf159-744f-4d5c-8869-80d0479a017d>
2014-04-18 13:39:06 +0000

    New Expression of Effective Date Validation in Reference data Tab.

--- SCRIPTS/EXPRESSIONS/exprASIT_BANK_INFO_EffectiveDate_OnSubmit.js
+++ SCRIPTS/EXPRESSIONS/exprASIT_BANK_INFO_EffectiveDate_OnSubmit.js
@@ -39,6 +39,7 @@ for(var rowIndex=0; rowIndex<totalRowCount; rowIndex++){
 	variable1.message="";
 	if(variable0.value!=null && variable0.value*1==toPrecision(1) && variable1.value!=null && formatDate(variable1.value,'yyyy/MM/dd')<formatDate(variable2.getValue() ,'yyyy/MM/dd')){
 		variable1.message="Effective Date cannot be in past. Please change the date.";
+		form.message="Effective Date cannot be in past. Please change the date.";
 		form.blockSubmit=true;
 	}
 	expression.setReturn(rowIndex,variable1);
