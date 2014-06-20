b25fa10e45449283a3be8e9cbc725872227f730e
johnschomp <johnschomp@eddbf159-744f-4d5c-8869-80d0479a017d>
2014-06-20 17:48:28 +0000

    copyLicASI edit asi in new transaction

--- INCLUDES_CUSTOM/COPYLICASI.js
+++ INCLUDES_CUSTOM/COPYLICASI.js
@@ -1,29 +1,6 @@
 var cap = aa.env.getValue("cap");
-var info = aa.env.getValue("info");
+var item = aa.env.getValue("item");
+var value = aa.env.getValue("value");
 
-copyLicASI(cap,info);
-
-function copyLicASI(newCap, newAInfo) {
-    var ignoreArr = new Array();
-    var limitCopy = false;
-    if (arguments.length > 1) {
-        ignoreArr = arguments[1];
-        limitCopy = true;
-    }
-    for (var item in newAInfo) {
-        //Check list
-        if (limitCopy) {
-            var ignore = false;
-            for (var i = 0; i < ignoreArr.length; i++)
-                if (ignoreArr[i] == newAInfo[item].FieldName) {
-                    ignore = true;
-                    break;
-                }
-            if (ignore)
-                continue;
-        }
-		aa.appSpecificInfo.editSingleAppSpecific(newCap,newAInfo[item].FieldName,newAInfo[item].Value,null);
-    }
-}
-
+aa.appSpecificInfo.editSingleAppSpecific(cap,item,value,null);
 
