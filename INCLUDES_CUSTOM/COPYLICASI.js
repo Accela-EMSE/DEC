6db38b66755b3f4ca6e5a73cd5bd11f89be27e2a
johnschomp <jschomp@accela.com>
2014-07-03 22:41:27 +0000

    update to use capidstring instead of cap object.   test to see if Object cannot be serialized error message is resolved

--- INCLUDES_CUSTOM/COPYLICASI.js
+++ INCLUDES_CUSTOM/COPYLICASI.js
@@ -1,6 +1,12 @@
-var cap = aa.env.getValue("cap");
+var capidstring = aa.env.getValue("cap");
 var item = aa.env.getValue("item");
 var value = aa.env.getValue("value");
 
-aa.appSpecificInfo.editSingleAppSpecific(cap,item,value,null);
-
+var capIdResult = aa.cap.getCapID(capidstring);
+if (capIdResult.getSuccess()) {
+	var editCapId = capIdResult.getOutput();
+	if (editCapId) {
+		aa.appSpecificInfo.editSingleAppSpecific(editCapId,item,value,null);
+		}
+	}
+	
