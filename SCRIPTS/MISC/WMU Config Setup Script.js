6848d4009b1c30cbbb30e1ba7fa4c4d7c844e27f
johnschomp <johnschomp@eddbf159-744f-4d5c-8869-80d0479a017d>
2014-08-04 04:34:20 +0000

    script for force open/closed WMUs when the product won't let you edit drop down fields.

---
+++ SCRIPTS/MISC/WMU Config Setup Script.js
@@ -0,0 +1,111 @@
+editLookupAuditStatus("WMU Choice 1","1A","I");
+editLookupAuditStatus("WMU Choice 1","1A","I");
+editLookupAuditStatus("WMU Choice 1","2A","I");
+editLookupAuditStatus("WMU Choice 1","3A","I");
+editLookupAuditStatus("WMU Choice 1","4L","I");
+editLookupAuditStatus("WMU Choice 1","5A","I");
+editLookupAuditStatus("WMU Choice 1","5C","I");
+editLookupAuditStatus("WMU Choice 1","5F","I");
+editLookupAuditStatus("WMU Choice 1","5G","I");
+editLookupAuditStatus("WMU Choice 1","5H","I");
+editLookupAuditStatus("WMU Choice 1","5J","I");
+editLookupAuditStatus("WMU Choice 1","6A","I");
+editLookupAuditStatus("WMU Choice 1","6F","I");
+editLookupAuditStatus("WMU Choice 1","6J","I");
+editLookupAuditStatus("WMU Choice 1","6N","I");
+editLookupAuditStatus("WMU Choice 1","9C","I");
+editLookupAuditStatus("WMU Choice 1","1C","A");
+editLookupAuditStatus("WMU Choice 1","3C","A");
+editLookupAuditStatus("WMU Choice 1","3F","A");
+editLookupAuditStatus("WMU Choice 1","3G","A");
+editLookupAuditStatus("WMU Choice 1","3H","A");
+editLookupAuditStatus("WMU Choice 1","3J","A");
+editLookupAuditStatus("WMU Choice 1","3K","A");
+editLookupAuditStatus("WMU Choice 1","3M","A");
+editLookupAuditStatus("WMU Choice 1","3N","A");
+editLookupAuditStatus("WMU Choice 1","3P","A");
+editLookupAuditStatus("WMU Choice 1","3R","A");
+editLookupAuditStatus("WMU Choice 1","3S","A");
+editLookupAuditStatus("WMU Choice 1","4A","A");
+editLookupAuditStatus("WMU Choice 1","4B","A");
+editLookupAuditStatus("WMU Choice 1","4C","A");
+editLookupAuditStatus("WMU Choice 1","4F","A");
+editLookupAuditStatus("WMU Choice 1","4G","A");
+editLookupAuditStatus("WMU Choice 1","4H","A");
+editLookupAuditStatus("WMU Choice 1","4J","A");
+editLookupAuditStatus("WMU Choice 1","4K","A");
+editLookupAuditStatus("WMU Choice 1","4O","A");
+editLookupAuditStatus("WMU Choice 1","4P","A");
+editLookupAuditStatus("WMU Choice 1","4R","A");
+editLookupAuditStatus("WMU Choice 1","4S","A");
+editLookupAuditStatus("WMU Choice 1","4T","A");
+editLookupAuditStatus("WMU Choice 1","4U","A");
+editLookupAuditStatus("WMU Choice 1","4W","A");
+editLookupAuditStatus("WMU Choice 1","4Y","A");
+editLookupAuditStatus("WMU Choice 1","4Z","A");
+editLookupAuditStatus("WMU Choice 1","5R","A");
+editLookupAuditStatus("WMU Choice 1","5S","A");
+editLookupAuditStatus("WMU Choice 1","5T","A");
+editLookupAuditStatus("WMU Choice 1","6C","A");
+editLookupAuditStatus("WMU Choice 1","6G","A");
+editLookupAuditStatus("WMU Choice 1","6H","A");
+editLookupAuditStatus("WMU Choice 1","6K","A");
+editLookupAuditStatus("WMU Choice 1","6P","A");
+editLookupAuditStatus("WMU Choice 1","6R","A");
+editLookupAuditStatus("WMU Choice 1","6S","A");
+editLookupAuditStatus("WMU Choice 1","7A","A");
+editLookupAuditStatus("WMU Choice 1","7F","A");
+editLookupAuditStatus("WMU Choice 1","7H","A");
+editLookupAuditStatus("WMU Choice 1","7J","A");
+editLookupAuditStatus("WMU Choice 1","7M","A");
+editLookupAuditStatus("WMU Choice 1","7P","A");
+editLookupAuditStatus("WMU Choice 1","7R","A");
+editLookupAuditStatus("WMU Choice 1","7S","A");
+editLookupAuditStatus("WMU Choice 1","8A","A");
+editLookupAuditStatus("WMU Choice 1","8C","A");
+editLookupAuditStatus("WMU Choice 1","8F","A");
+editLookupAuditStatus("WMU Choice 1","8G","A");
+editLookupAuditStatus("WMU Choice 1","8H","A");
+editLookupAuditStatus("WMU Choice 1","8J","A");
+editLookupAuditStatus("WMU Choice 1","8M","A");
+editLookupAuditStatus("WMU Choice 1","8N","A");
+editLookupAuditStatus("WMU Choice 1","8P","A");
+editLookupAuditStatus("WMU Choice 1","8R","A");
+editLookupAuditStatus("WMU Choice 1","8S","A");
+editLookupAuditStatus("WMU Choice 1","8T","A");
+editLookupAuditStatus("WMU Choice 1","8W","A");
+editLookupAuditStatus("WMU Choice 1","8X","A");
+editLookupAuditStatus("WMU Choice 1","8Y","A");
+editLookupAuditStatus("WMU Choice 1","9A","A");
+editLookupAuditStatus("WMU Choice 1","9F","A");
+editLookupAuditStatus("WMU Choice 1","9G","A");
+editLookupAuditStatus("WMU Choice 1","9H","A");
+editLookupAuditStatus("WMU Choice 1","9J","A");
+editLookupAuditStatus("WMU Choice 1","9K","A");
+editLookupAuditStatus("WMU Choice 1","9M","A");
+editLookupAuditStatus("WMU Choice 1","9N","A");
+editLookupAuditStatus("WMU Choice 1","9P","A");
+editLookupAuditStatus("WMU Choice 1","9R","A");
+editLookupAuditStatus("WMU Choice 1","9S","A");
+editLookupAuditStatus("WMU Choice 1","9T","A");
+editLookupAuditStatus("WMU Choice 1","9W","A");
+editLookupAuditStatus("WMU Choice 1","9X","A");
+editLookupAuditStatus("WMU Choice 1","9Y","A");
+
+
+function editLookupAuditStatus(stdChoice, stdValue, stdAuditStaus) {
+    //check if stdChoice and stdValue already exist; if they do, update;
+    var bizDomScriptResult = aa.bizDomain.getBizDomainByValue(stdChoice, stdValue);
+    if (bizDomScriptResult.getSuccess()) {
+        bds = bizDomScriptResult.getOutput();
+
+        var bd = bds.getBizDomain()
+        bd.setAuditStatus(stdAuditStaus);
+        var editResult = aa.bizDomain.editBizDomain(bd)
+
+        if (editResult.getSuccess())
+            aa.print("Successfully edited Std Choice Audit Status(" + stdChoice + "," + stdValue + ") = " + stdAuditStaus);
+        else
+            aa.print("**WARNING editing Std Choice  Audit Statu" + editResult.getErrorMessage());
+    }
+}
